ffba35d9e6591f08aa15ed680ef85c62476259ab
ajschilt <ajschilt86@gmail.com>
2018-07-16 15:19:20 -0500

    able to pick r p or s

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -23,24 +23,25 @@ $("#btn-name").click(function () {
     if (player1 === false) {
         player1 = $("#inputName").val().trim();
 
-        database.ref().set({
+        database.ref("connections/player1").set({
             player1: player1
         });
 
-        database.ref().on("value", function(snapshot){
-            $(".p1Name").html("<p>" + snapshot.val().player1 + "</p>");
-        })
-    
+        database.ref().on("value", function (snapshot) {
+            $(".p1Name").html("<p>" + snapshot.val().connections.player1.player1 + "</p>");
+        });
+
     } else if (player2 === false) {
         player2 = $("#inputName").val().trim();
 
-        database.ref().set({
+        database.ref("connections/player2").set({
             player2: player2
         });
 
-        database.ref().on("value", function(snapshot){
-            $(".p2Name").html("<p>" + snapshot.val().player2 + "</p>");
-        })
+        database.ref().on("value", function (snapshot) {
+            $(".p2Name").html("<p>" + snapshot.val().connections.player2.player2 + "</p>");
+        });
+        game();
     }
 
     var addName = $(".form-control").val().trim();
@@ -51,39 +52,40 @@ $("#btn-name").click(function () {
         document.forms["inputForm"].reset();
     }
 
-    
+
 
 });
 
 $(document).keydown(function (e) {
     var key_one = 13;
-  
+
     if (e.keyCode == key_one) {
         event.preventDefault();
 
         if (player1 === false) {
             player1 = $("#inputName").val().trim();
-    
+
             database.ref("connections/player1").set({
                 player1: player1
             });
-    
-            database.ref().on("value", function(snapshot){
+
+            database.ref().on("value", function (snapshot) {
                 $(".p1Name").html("<p>" + snapshot.val().connections.player1.player1 + "</p>");
-            })
-        
+            });
+
         } else if (player2 === false) {
             player2 = $("#inputName").val().trim();
-    
+
             database.ref("connections/player2").set({
                 player2: player2
             });
-    
-            database.ref().on("value", function(snapshot){
+
+            database.ref().on("value", function (snapshot) {
                 $(".p2Name").html("<p>" + snapshot.val().connections.player2.player2 + "</p>");
-            })
+            });
+            game();
         }
-    
+
         var addName = $(".form-control").val().trim();
         if (addName === "") {
             return false;
@@ -92,7 +94,45 @@ $(document).keydown(function (e) {
             document.forms["inputForm"].reset();
         }
     }
-  });
+});
+
+var player1Choice = false;
+var player2Choice = false;
+
+function game() {
+    if (player1Choice === false) {
+        $(".rock").click(function () {
+            player1Choice = "rock"
+            $(".prompt1").html("<p> player one chose " + player1Choice);
+            return false;
+        });
+        $(".paper").click(function () {
+            player1Choice = "paper"
+            $(".prompt1").html("<p> player one chose " + player1Choice);
+        });
+        $(".scissors").click(function () {
+            player1Choice = "scissors"
+            $(".prompt1").html("<p> player one chose " + player1Choice);
+        });
+    }
+    if (player2Choice === false) {
+        $(".rock2").click(function () {
+            player2Choice = "rock"
+            $(".prompt2").html("<p> player two chose " + player2Choice);
+        });
+        $(".paper2").click(function () {
+            player2Choice = "paper"
+            $(".prompt2").html("<p> player two chose " + player2Choice);
+        });
+        $(".scissors2").click(function () {
+            player2Choice = "scissors"
+            $(".prompt2").html("<p> player two chose " + player2Choice);
+        });
+
+        console.log(player1Choice);
+    }
+}
+
 
 
 
